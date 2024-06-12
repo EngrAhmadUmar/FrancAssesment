531e49d5e5fa3c9c3122e9a40fb7129e5d2d7d9d
unknown <zcaptain2002@gmail.com>
2024-06-12 15:01:19 +0200

    Display Posts For Each User in Chronological Order

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template, jsonify, Response, request
+import json
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index_view():
     username = request.args.get('username')
-    return render_template('index.html', username = username)
+    posts = []
+    
+    # Read users from users.json
+    with open('./users.json', 'r') as f:
+        users_data = json.load(f)
+        users = list(users_data.keys())
+    
+    # If a username is provided, get their posts
+    if username:
+        with open('./posts.json', 'r') as f:
+            posts_data = json.load(f)
+            posts = posts_data.get(username, [])
+            
+            # Sort posts by time in descending order (most recent first)
+            posts.sort(key=lambda x: datetime.strptime(x['time'], '%Y-%m-%dT%H:%M:%SZ'), reverse=True)
+            
+    return render_template('index.html', username=username, posts=posts, users=users)
 
 @app.route('/users')
 def users_view():
